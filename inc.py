44245071ca84e57675507a02c9d05c8eb393ad18
fartun <fartunomar2345@gmail.com>
2023-08-02 17:58:35 +0300

    excercise

---
+++ inc.py
@@ -0,0 +1,6 @@
+''' checking for inclusion
+'''
+name=input("enter your name: ")
+inc=input("check if your name has -es: ")
+print("es" in inc)
+
--- m.py
+++ m.py
@@ -1,3 +1,5 @@
+'''input for a string of list eg 2009 blue chevrolet silverado
+'''
 year=int(input("enter the year: "))
 color=input("enter the color: ")
 make=input("enter the make: ")
